02eb30e35e762bf3c70491819bb036cdb8f2fb4d
Justin Rodenbostel <justin@rodenbostel.com>
2015-06-24 11:53:36 -0500

    initial revision

---
+++ src/main/java/cjugFirmata4jDemo.groovy
@@ -0,0 +1,40 @@
+import org.firmata4j.IODevice
+import org.firmata4j.firmata.FirmataDevice
+
+import static org.firmata4j.Pin.Mode.OUTPUT
+
+/**
+ * Created by justin on 6/24/15.
+ */
+class cjugFirmata4jDemo {
+
+    static IODevice device = new FirmataDevice("/dev/tty.usbmodem1451");
+
+    def static main(String[] args) throws IOException, InterruptedException {
+        try {
+            setup();
+            loop();
+        }
+        finally {
+            device.stop();
+        }
+    }
+
+    def static setup() {
+        device.start() // initiate communication to the device
+        device.ensureInitializationIsDone() // wait for initialization is done
+        println "Device is ready"
+    }
+
+    def static loop() {
+        while (true) {
+            def pin = device.getPin 13;
+            pin.setMode OUTPUT
+
+            pin.setValue 1
+            Thread.sleep 500
+            pin.setValue 0
+            Thread.sleep 500
+        }
+    }
+}
